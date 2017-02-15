2d6c0c1e14b9aee700dfef750cc73b4508868d9a
James Harris <james.harris@icecave.com.au>
2017-02-16 09:45:09 +1000

    Add `AttrTableWithMetaData.Clone()` and `MatchConstraint()`.

--- src/internals/attr_meta_data.go
+++ src/internals/attr_meta_data.go
@@ -11,3 +11,25 @@ type AttrWithMetaData struct {
 
 // AttrTableWithMetaData maps attribute keys to attributes with meta data.
 type AttrTableWithMetaData map[string]AttrWithMetaData
+
+// Clone returns a copy of the attribute table.
+func (t AttrTableWithMetaData) Clone() AttrTableWithMetaData {
+	r := AttrTableWithMetaData{}
+
+	for k, v := range t {
+		r[k] = v
+	}
+
+	return r
+}
+
+// MatchConstraint returns true if the attributes match the given constraint.
+func (t AttrTableWithMetaData) MatchConstraint(constraint overpass.Constraint) bool {
+	for key, value := range constraint {
+		if t[key].Value != value {
+			return false
+		}
+	}
+
+	return true
+}
