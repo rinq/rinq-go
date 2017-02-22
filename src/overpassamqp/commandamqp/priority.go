8dea2514756805cc7cd8ad5756e65bfc3eff1326
James Harris <james.harris@icecave.com.au>
2017-02-23 08:36:52 +1000

    Use priority queues and elevate unicast call priority above others.

--- src/overpassamqp/commandamqp/priority.go
+++ src/overpassamqp/commandamqp/priority.go
@@ -2,14 +2,20 @@ package commandamqp
 
 const (
 	// executePriority is the AMQP priority for "Execute*" operations.
-	executePriority = iota
+	executePriority uint8 = iota
 
-	// callPriority is the AMQP priority for "Call*" operations. These
-	// operations always have a timeout, so the priority is raised above
+	// callBalancedPriority is the AMQP priority for "CallBalanced" operations.
+	// These operations always have a timeout, so the priority is raised above
 	// operations that don't.
-	callPriority
+	callBalancedPriority
+
+	// callUnicastPriority is the AMQP priority for "CallUnicast" operations.
+	// Like CallBalanced, these operations have a timeout. They are also used
+	// to implement internal features, and so the priority is raised even higher
+	// again.
+	callUnicastPriority
 
 	// priorityCount is the number of priorities in use, used to declare the
-	// AMQP queues with the exact number of priority slots. // TODO
+	// AMQP queues with the exact number of priority slots.
 	priorityCount
 )
