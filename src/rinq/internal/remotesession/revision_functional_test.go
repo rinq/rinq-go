6356d20628ea97ecbb7dae90680ce6507a2d31fc
James Harris <james.harris@icecave.com.au>
2017-10-02 16:10:50 +1000

    Add some more functional tests for `remotesession.revision`.

--- src/rinq/internal/remotesession/revision_functional_test.go
+++ src/rinq/internal/remotesession/revision_functional_test.go
@@ -11,7 +11,6 @@ import (
 	"github.com/rinq/rinq-go/src/rinq/internal/testutil"
 )
 
-//
 var _ = Describe("revision (functional)", func() {
 	var (
 		ctx            context.Context
@@ -62,10 +61,58 @@ var _ = Describe("revision (functional)", func() {
 			Expect(err).NotTo(HaveOccurred())
 			Expect(remote.Ref()).To(Equal(local.Ref()))
 		})
+
+		It("returns a not found error if the session has been destroyed", func() {
+			session.Destroy()
+
+			_, err := remote.Refresh(ctx)
+			Expect(err).To(HaveOccurred())
+			Expect(rinq.IsNotFound(err)).To(BeTrue())
+		})
 	})
 
 	Describe("Get", func() {
-		It("returns a previously updated value from the cache", func() {
+		It("returns an empty attribute at revision zero", func() {
+			attr, err := remote.Get(ctx, ns, "a")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(attr).To(Equal(rinq.Set("a", "")))
+		})
+
+		It("returns an empty attribute at revision zero after the session is destroyed", func() {
+			session.Destroy()
+
+			attr, err := remote.Get(ctx, ns, "a")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(attr).To(Equal(rinq.Set("a", "")))
+		})
+
+		It("returns an empty attribute when none exists", func() {
+			var err error
+			local, err = local.Update(ctx, ns, rinq.Set("a", "1"))
+			Expect(err).NotTo(HaveOccurred())
+
+			remote, err = remote.Refresh(ctx)
+			Expect(err).NotTo(HaveOccurred())
+
+			attr, err := remote.Get(ctx, ns, "b")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(attr).To(Equal(rinq.Set("b", "")))
+		})
+
+		It("returns an attribute created on the owning peer", func() {
+			var err error
+			local, err = local.Update(ctx, ns, rinq.Set("a", "1"))
+			Expect(err).NotTo(HaveOccurred())
+
+			remote, err = remote.Refresh(ctx)
+			Expect(err).NotTo(HaveOccurred())
+
+			attr, err := remote.Get(ctx, ns, "a")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(attr).To(Equal(rinq.Set("a", "1")))
+		})
+
+		It("returns an attribute updated on the remote peer from the cache", func() {
 			// setup a handler that updates an attribute remotely
 			testutil.Must(server.Listen(ns, func(ctx context.Context, req rinq.Request, res rinq.Response) {
 				var err error
@@ -91,6 +138,188 @@ var _ = Describe("revision (functional)", func() {
 			attr, err := remote.Get(ctx, ns, "a")
 			Expect(err).NotTo(HaveOccurred())
 			Expect(attr.Value).To(Equal("1"))
+		})
+
+		It("returns a stale fetch error if the attribute has been updated in a later revision", func() {
+			var err error
+			local, err = local.Update(ctx, ns, rinq.Set("a", "1"))
+			Expect(err).NotTo(HaveOccurred())
+
+			remote, err = remote.Refresh(ctx)
+			Expect(err).NotTo(HaveOccurred())
+
+			local, err = local.Update(ctx, ns, rinq.Set("a", "2"))
+			Expect(err).NotTo(HaveOccurred())
+
+			_, err = remote.Get(ctx, ns, "a")
+			Expect(err).To(HaveOccurred())
+			Expect(rinq.ShouldRetry(err)).To(BeTrue())
+		})
+
+		It("returns a not found error if the session has been destroyed", func() {
+			// bump the version otherwise Get knows to return an empty attribute
+			// for revision zero.
+			var err error
+			local, err = local.Update(ctx, ns, rinq.Set("a", "1"))
+			Expect(err).NotTo(HaveOccurred())
+
+			remote, err = remote.Refresh(ctx)
+			Expect(err).NotTo(HaveOccurred())
+
+			session.Destroy()
+
+			_, err = remote.Get(ctx, ns, "a")
+			Expect(err).To(HaveOccurred())
+			Expect(rinq.IsNotFound(err)).To(BeTrue())
+		})
+	})
+
+	Describe("GetMany", func() {
+		It("returns empty attributes at revision zero", func() {
+			attrs, err := remote.GetMany(ctx, ns, "a", "b")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(attrs).To(Equal(
+				rinq.AttrTable{
+					"a": {Key: "a"},
+					"b": {Key: "b"},
+				},
+			))
+		})
+
+		It("returns empty attributes at revision zero after the session is destroyed", func() {
+			session.Destroy()
+
+			attrs, err := remote.GetMany(ctx, ns, "a", "b")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(attrs).To(Equal(
+				rinq.AttrTable{
+					"a": {Key: "a"},
+					"b": {Key: "b"},
+				},
+			))
+		})
+
+		It("returns empty attributes when none exist", func() {
+			var err error
+			local, err = local.Update(ctx, ns, rinq.Set("a", "1"))
+			Expect(err).NotTo(HaveOccurred())
+
+			remote, err = remote.Refresh(ctx)
+			Expect(err).NotTo(HaveOccurred())
+
+			attrs, err := remote.GetMany(ctx, ns, "b", "c")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(attrs).To(Equal(
+				rinq.AttrTable{
+					"b": {Key: "b"},
+					"c": {Key: "c"},
+				},
+			))
+		})
+
+		It("returns an empty attribute table when called without keys", func() {
+			session.Destroy() // even if the session has been destroyed
+
+			attrs, err := remote.GetMany(ctx, ns)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(attrs).To(BeEmpty())
+		})
+
+		It("returns attributes created on the owning peer", func() {
+			var err error
+			local, err = local.Update(
+				ctx,
+				ns,
+				rinq.Set("a", "1"),
+				rinq.Set("b", "2"),
+			)
+			Expect(err).NotTo(HaveOccurred())
+
+			remote, err = remote.Refresh(ctx)
+			Expect(err).NotTo(HaveOccurred())
+
+			attrs, err := remote.GetMany(ctx, ns, "a", "b")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(attrs).To(Equal(
+				rinq.AttrTable{
+					"a": rinq.Set("a", "1"),
+					"b": rinq.Set("b", "2"),
+				},
+			))
+		})
+
+		It("returns a stale fetch error if the attribute has been updated in a later revision", func() {
+			var err error
+			local, err = local.Update(ctx, ns, rinq.Set("a", "1"))
+			Expect(err).NotTo(HaveOccurred())
+
+			remote, err = remote.Refresh(ctx)
+			Expect(err).NotTo(HaveOccurred())
+
+			local, err = local.Update(ctx, ns, rinq.Set("a", "2"))
+			Expect(err).NotTo(HaveOccurred())
+
+			_, err = remote.GetMany(ctx, ns, "a")
+			Expect(err).To(HaveOccurred())
+			Expect(rinq.ShouldRetry(err)).To(BeTrue())
+		})
+
+		It("returns a not found error if the session has been destroyed", func() {
+			// bump the version otherwise Get knows to return an empty attribute
+			// for revision zero.
+			var err error
+			local, err = local.Update(ctx, ns, rinq.Set("a", "1"))
+			Expect(err).NotTo(HaveOccurred())
+
+			remote, err = remote.Refresh(ctx)
+			Expect(err).NotTo(HaveOccurred())
+
+			session.Destroy()
+
+			_, err = remote.GetMany(ctx, ns, "a")
+			Expect(err).To(HaveOccurred())
+			Expect(rinq.IsNotFound(err)).To(BeTrue())
+		})
+	})
+
+	Describe("Update", func() {
+		It("returns a stale update error if session is at a later revision", func() {
+			var err error
+			local, err = local.Update(ctx, ns, rinq.Set("a", "1"))
+			Expect(err).NotTo(HaveOccurred())
+
+			_, err = remote.Update(ctx, ns, rinq.Set("a", "2"))
+			Expect(err).To(HaveOccurred())
+			Expect(rinq.ShouldRetry(err)).To(BeTrue())
+		})
+
+		It("returns a not found error if the session has been destroyed", func() {
+			session.Destroy()
+
+			var err error
+			remote, err = remote.Update(ctx, ns, rinq.Set("a", "1"))
+			Expect(err).To(HaveOccurred())
+			Expect(rinq.IsNotFound(err)).To(BeTrue())
+		})
+	})
+
+	Describe("Destroy", func() {
+		It("returns a stale update error if session is at a later revision", func() {
+			var err error
+			local, err = local.Update(ctx, ns, rinq.Set("a", "1"))
+			Expect(err).NotTo(HaveOccurred())
+
+			err = remote.Destroy(ctx)
+			Expect(err).To(HaveOccurred())
+			Expect(rinq.ShouldRetry(err)).To(BeTrue())
+		})
+
+		It("returns a not found error if the session has been destroyed", func() {
+			session.Destroy()
+
+			err := remote.Destroy(ctx)
+			Expect(err).To(HaveOccurred())
+			Expect(rinq.IsNotFound(err)).To(BeTrue())
 		})
 	})
 
