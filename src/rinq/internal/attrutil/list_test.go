0e0545c15d949762b9377dc37b74f6c12c8e81df
James Harris <james.harris@icecave.com.au>
2017-10-03 22:36:40 +1000

    Use unqualified import for package-under-test.

--- src/rinq/internal/attrutil/list_test.go
+++ src/rinq/internal/attrutil/list_test.go
@@ -6,7 +6,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/rinq/rinq-go/src/rinq"
-	"github.com/rinq/rinq-go/src/rinq/internal/attrutil"
+	. "github.com/rinq/rinq-go/src/rinq/internal/attrutil"
 )
 
 var _ = Describe("List", func() {
@@ -14,14 +14,14 @@ var _ = Describe("List", func() {
 		It("writes only braces when the list is empty", func() {
 			var buf bytes.Buffer
 
-			attrutil.List{}.WriteTo(&buf)
+			List{}.WriteTo(&buf)
 
 			Expect(buf.String()).To(Equal("{}"))
 		})
 
 		It("writes key/value pairs in order", func() {
 			var buf bytes.Buffer
-			l := attrutil.List{
+			l := List{
 				rinq.Set("a", "1"),
 				rinq.Set("b", "2"),
 			}
@@ -34,11 +34,11 @@ var _ = Describe("List", func() {
 
 	Describe("String", func() {
 		It("returns only braces when the list is empty", func() {
-			Expect(attrutil.List{}.String()).To(Equal("{}"))
+			Expect(List{}.String()).To(Equal("{}"))
 		})
 
 		It("returns key/value pairs in order", func() {
-			l := attrutil.List{
+			l := List{
 				rinq.Set("a", "1"),
 				rinq.Set("b", "2"),
 			}
