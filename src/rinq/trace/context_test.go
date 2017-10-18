3eaf493392c32453961fa03d3523ef990bb75cae
James Harris <james.harris@icecave.com.au>
2017-10-18 14:53:26 +1000

    Add `trace.WithRoot()` function.

--- src/rinq/trace/context_test.go
+++ src/rinq/trace/context_test.go
@@ -8,13 +8,39 @@ import (
 	. "github.com/rinq/rinq-go/src/rinq/trace"
 )
 
-var _ = Describe("With and Get", func() {
-	It("transports the trace ID in the context", func() {
+var _ = Describe("With", func() {
+	It("adds the trace ID", func() {
 		ctx := With(context.Background(), "<id>")
 
 		Expect(Get(ctx)).To(Equal("<id>"))
 	})
 
+	It("replaces an existing trace ID", func() {
+		parent := With(context.Background(), "<id 1>")
+		ctx := With(parent, "<id 2>")
+
+		Expect(Get(ctx)).To(Equal("<id 2>"))
+	})
+})
+
+var _ = Describe("WithRoot", func() {
+	It("adds the trace ID", func() {
+		ctx, added := WithRoot(context.Background(), "<id>")
+
+		Expect(Get(ctx)).To(Equal("<id>"))
+		Expect(added).To(BeTrue())
+	})
+
+	It("returns the parent context unchanged", func() {
+		parent := With(context.Background(), "<id 1>")
+		ctx, added := WithRoot(parent, "<id 2>")
+
+		Expect(ctx).To(BeIdenticalTo(parent))
+		Expect(added).To(BeFalse())
+	})
+})
+
+var _ = Describe("Get", func() {
 	It("returns an empty string when no trace ID is present", func() {
 		ctx := context.Background()
 
