2c5c43e43dcd3a6888eec93981fd776e8aa541ae
James Harris <james.harris@icecave.com.au>
2017-10-27 10:23:29 +1000

    Add `namespaces.MustValidate()`.

--- src/internal/namespaces/validate_test.go
+++ src/internal/namespaces/validate_test.go
@@ -6,8 +6,17 @@ import (
 	"github.com/rinq/rinq-go/src/internal/namespaces"
 )
 
+var entries = []TableEntry{
+	Entry("all valid characters", ":Aa3-_.", ""),
+	Entry("typical style", "foo.bar.v1", ""),
+	Entry("empty", "", "namespace must not be empty"),
+	Entry("underscore", "_", "namespace '_' is reserved"),
+	Entry("leading underscore", "_foo", "namespace '_foo' is reserved"),
+	Entry("invalid characters", "foo bar", "namespace 'foo bar' contains invalid characters"),
+}
+
 var _ = DescribeTable(
-	"ValidateNamespace",
+	"Validate",
 	func(namespace string, expected string) {
 		err := namespaces.Validate(namespace)
 
@@ -17,10 +26,21 @@ var _ = DescribeTable(
 			Expect(err.Error()).To(Equal(expected))
 		}
 	},
-	Entry("all valid characters", ":Aa3-_.", ""),
-	Entry("typical style", "foo.bar.v1", ""),
-	Entry("empty", "", "namespace must not be empty"),
-	Entry("underscore", "_", "namespace '_' is reserved"),
-	Entry("leading underscore", "_foo", "namespace '_foo' is reserved"),
-	Entry("invalid characters", "foo bar", "namespace 'foo bar' contains invalid characters"),
+	entries...,
+)
+
+var _ = DescribeTable(
+	"MustValidate",
+	func(namespace string, expected string) {
+		fn := func() {
+			namespaces.MustValidate(namespace)
+		}
+
+		if expected == "" {
+			Expect(fn).ShouldNot(Panic())
+		} else {
+			Expect(fn).Should(Panic())
+		}
+	},
+	entries...,
 )
