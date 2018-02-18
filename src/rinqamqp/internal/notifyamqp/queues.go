5b2691abb89aca01862fb806d2b4706338b620b7
James Harris <james.harris@icecave.com.au>
2018-02-18 10:50:16 +1000

    Remove old `notifyamqp` package.

--- src/rinqamqp/internal/notifyamqp/queues.go
+++
@@ -1,8 +0,0 @@
-package notifyamqp
-
-import "github.com/rinq/rinq-go/src/rinq/ident"
-
-// notifyQueue returns the name of the queue used for incoming notifications.
-func notifyQueue(id ident.PeerID) string {
-	return id.ShortString() + ".ntf"
-}
